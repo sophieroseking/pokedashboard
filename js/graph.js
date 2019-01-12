3b0ec247ba9d73c1a0f1cefa7efcec9e732a95f0
Sophie King <sophiek89@hotmail.com>
2019-01-12 00:18:45 +0000

    Uploaded dc d3 js and css files, basic html and css in place. Linked csv file and added a few percentage buttons and a bar chart

---
+++ js/graph.js
@@ -0,0 +1,200 @@
+queue()
+    .defer(d3.csv, "data/pokedex.csv")
+    .await(makeGraphs);
+
+function makeGraphs(error, pokemonData) {
+    var ndx = crossfilter(pokemonData);
+
+
+    show_region_selector(ndx);
+    show_region_percentage(ndx);
+    show_type_percentage(ndx);
+    show_grass_pokemon_in_region(ndx, "Kanto", "#percent-of-kanto-pokemon");
+    show_grass_pokemon_in_region(ndx, "Johto", "#percent-of-johto-pokemon");
+    show_grass_pokemon_in_region(ndx, "Hoenn", "#percent-of-hoenn-pokemon");
+    show_grass_pokemon_in_region(ndx, "Sinnoh", "#percent-of-sinnoh-pokemon");
+    show_grass_pokemon_in_region(ndx, "Unova", "#percent-of-unova-pokemon");
+    show_grass_pokemon_in_region(ndx, "Kalos", "#percent-of-kalos-pokemon");
+    show_grass_pokemon_in_region(ndx, "Alola", "#percent-of-alola-pokemon");
+    
+    dc.renderAll();
+}
+
+
+function show_region_selector(ndx) {
+    var regionDim = ndx.dimension(dc.pluck("region"));
+    var regionSelect = regionDim.group();
+
+    dc.selectMenu("#region-selector")
+        .dimension(regionDim)
+        .group(regionSelect);
+}
+
+function show_type_percentage(ndx, type_1) {
+    var typeColors = d3.scale.ordinal()
+        .domain(["bug", "fire"])
+        .range(["pink", "blue"]);
+    var typeDim = ndx.dimension(function(d) {
+        return [d.type_1];
+    });
+    var typeMix = typeDim.group();
+
+ dc.pieChart("#type-percentage")
+        .height(150)
+        .radius(75)
+        .transitionDuration(500)
+        .colorAccessor(function(d) { return d.key[0]; })
+        .colors(typeColors)
+        .dimension(typeDim)
+        .group(typeMix)
+}
+
+function show_grass_pokemon_in_region(ndx, place, element) {
+    var region_dim = ndx.dimension(dc.pluck('region'));
+    var total_grass_pokemon = ndx.groupAll().reduce(
+        function(p, v) {
+            if (v.region === place) {
+                p.count++;
+                if (v.type_1 === "Grass") {
+                    p.are_grass++;
+                }
+            }
+            return p;
+        },
+        function(p, v) {
+            if (v.region === place) {
+                p.count--;
+                if (v.type_1 === "Grass") {
+                    p.are_grass--;
+                }
+            }
+            return p;
+        },
+        function() {
+            return { count: 0, are_grass: 0 };
+        }
+    );
+
+    dc.numberDisplay(element)
+        .formatNumber(d3.format(".2%"))
+        .valueAccessor(function(d) {
+            if (d.count == 0) {
+                return 0;
+            }
+            else {
+                return (d.are_grass / d.count);
+            }
+        })
+        .group(total_grass_pokemon);
+        
+
+}
+
+
+/*
+
+function show_number_per_region(ndx) {
+    var regionColors = d3.scale.ordinal()
+        .domain(["A", "B", "C", "D", "E"])
+        .range(["red", "orange", "yellow", "green", "blue"]);
+    var number_regionDim = ndx.dimension(function(d) {
+        return [d.number_region];
+    });
+    var number_regionMix = number_regionDim.group();
+
+    dc.barChart("#number-per-region")
+        .width(350)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .colorAccessor(function(d, i) { return i; })
+        .colors(regionColors)
+        .dimension(number_regionDim)
+        .group(number_regionMix)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Region")
+        .yAxis().ticks(5);
+}
+*/
+
+
+/*
+function show_percent_that_are_in_each_region(ndx) {
+
+    function type_in_region(dimension, type_1) {
+        return dimension.group().reduce(
+            function(p, v) {
+                p.total++;
+                if (v.type_1 === type_1) {
+                    p.match++;
+                };
+                return p;
+            },
+            function(p, v) {
+                p.total--;
+                if (v.type_1 === type_1) {
+                    p.match--;
+                };
+                return p;
+            },
+            function() {
+                return { total: 0, match: 0 }
+            }
+        );
+    };
+
+    var dim = ndx.dimension(dc.pluck("type_1"));
+    var regionColors = d3.scale.ordinal()
+        .domain(["Kanto", "Johto", "Sinnoh", "Unova", "Kalos"])
+        .range(["red", "orange", "yellow", "green", "blue"]);
+    var region_typeDim = ndx.dimension(function(d) {
+        return [d.region_type];
+    });
+    var region_typeMix = region_typeDim.group();
+
+
+    dc.barChart("#type_in_region")
+        .width(400)
+        .height(350)
+        .margins({ top: 10, right: 100, bottom: 30, left: 30 })
+        .colorAccessor(function(d, i) { return i; })
+        .colors(regionColors)
+        .dimension(region_typeDim)
+        .group(region_typeMix)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Region")
+        .yAxis().ticks(5);
+}
+
+*/
+
+    
+
+function show_region_percentage(ndx) {
+    var regionColors = d3.scale.ordinal()
+        .domain(["Kanto", "Johto", "Hoenn", "Sinnoh", "Unova", "Kalos", "Alola"])
+        .range(["red", "orange", "yellow", "green", "blue", "purple", "pink"]);
+    var regionDim = ndx.dimension(function(d) {
+        return [d.region];
+    });
+    var regionMix = regionDim.group();
+
+    dc.barChart("#region-balance")
+        .width(350)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .colorAccessor(function(d) { return d.key[0]; })
+        .colors(regionColors)
+        .dimension(regionDim)
+        .group(regionMix)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Region")
+        .yAxis().ticks(20);
+}
